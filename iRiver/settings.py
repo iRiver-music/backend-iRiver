e268046de326ced8aa1d31858143621f83c0137a
weizijun <zijun1152@gmail.com>
2023-07-07 15:06:19 +0800

    Chore : change .gitignore - 將iRiver的__pycache__加入至gitignore中

--- iRiver/settings.py
+++ iRiver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'User',
     'Token',
     'Discover',
-    'corsheaders',
+    #'corsheaders',
 ]
 
 REST_FRAMEWORK = {
