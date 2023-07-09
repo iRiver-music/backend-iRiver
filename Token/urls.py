0eb2403a6b911b09e97651d9932669190ee780ea
林立純 <allen@linlichundeMacBook-Pro.local>
2023-07-09 10:59:07 +0800

    Feat: add Token's routes 'token/' and 'token/refresh' -新增Token/中的路由

--- Token/urls.py
+++ Token/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path ,include
 from . import views
+from .views import UserTokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 app_name = 'Token'
 urlpatterns = [
-
+    path('token/', UserTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
