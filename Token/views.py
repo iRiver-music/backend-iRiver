009858a344cdd497dec05c22643c09673be39319
林立純 <allen@linlichundeMacBook-Pro.local>
2023-07-09 10:54:07 +0800

    Feat: add UserTokenObtainPairView -根據用戶名產生Access_Token和Refresh_Token，目前設定Access_Token的有效時間為5分鐘，Refresh_Token為1天。

--- Token/views.py
+++ Token/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+#自定義Token Serializer
+class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
+
+class UserTokenObtainPairView(TokenObtainPairView):
+    serializer_class = UserTokenObtainPairSerializer
