31c27da3089ab80a4a593d40f0727e5d8f61e77a
chris ho <98445729+ho0527@users.noreply.github.com>
2023-07-10 17:55:34 +0800

    modify: change sql connect way
    - 改成使用function來連接

--- User/sql.py
+++ User/sql.py
@@ -10,23 +10,22 @@ import pymysql
 dbuser=pymysql.connect(host="iriversql.ddns.net",port=3306,user="gWvPZkyaanAP5cXQqE8hkX5hnmYYhcMr",passwd="JABmQsQhpj05F6WI",db="django_user",charset="utf8") # user 的 db
 dbmusic=pymysql.connect(host="iriversql.ddns.net",port=3306,user="gWvPZkyaanAP5cXQqE8hkX5hnmYYhcMr",passwd="JABmQsQhpj05F6WI",db="music_db",charset="utf8") # muisc 的 db
 
-query=dbuser.cursor() # 操作游標
+# query 的簡化 function
+def query(db,query,data=()):
+    try:
+        row=db.cursor() # 操作游標
+        row.execute(query,data) # 匯入data
+        db.commit()
+        print("success!")
+    except Exception as e:
+        print("[ERROR]"+str(e))
 
-def loginsql(): # 登入用sql def
+    row.close()
+    db.close()
+    return row.fetchall()
+
+def loginsql(): # 登入用sql
     pass
-
-try:
-    query.execute("INSERT INTO `sqltest`(`test`,`test2`,`test3`)VALUES(%s,%s,%s)",("123","234","345")) # 執行語法
-    dbuser.commit() # 提交修改
-    print("success")
-except Exception as e:
-    # 發生錯誤時停止執行SQL
-    dbuser.rollback()
-    print("[ERROR]"+str(e))
-
-# 關閉連線
-dbuser.close()
-
 
 # 註記
 # 只要函式後面有加sql都是sql函式
