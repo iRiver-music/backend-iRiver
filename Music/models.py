0b08c166409f81de210d8021d1e7e3b2e081747a
weizijun <zijun1152@gmail.com>
2023-07-17 22:14:49 +0800

    Modify : change Artists class in music/models and add create_tables function in music/models - 將原本的Post class改成新的Artist class，並在music/models.py中加入了create_tables 函式去創建新的資料

--- Music/models.py
+++ Music/models.py
@@ -2,28 +2,45 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
-class Post(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.CharField(max_length=100)
-    body = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+class Artist(models.Model):
+    artist = models.CharField(max_length=255, unique=True)
+    summary = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('-pub_date' , )
+        app_label = 'music'
+        db_table = 'artists'
+
     def __str__(self):
-        return f"{self.name} ({self.pub_date})"
+        return self.artist
 
 class music(models.Model):
-    title = models.CharField(max_length=100)
-    song_ID = models.CharField(max_length=100)
-    song_img_url = models.CharField(max_length=100)
-    artist = models.CharField(max_length=100)
-    artist_url = models.CharField(max_length=100, default='')
-    artist_img_url = models.CharField(max_length=100)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    artist = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    music_ID = models.CharField(max_length=255, unique=True)
+    album = models.CharField(max_length=255)
+    label = models.CharField(max_length=255)
+    artist_url = models.CharField(max_length=255)
+    sources = models.CharField(max_length=255)
+    download_status = models.BooleanField(default=False)
+    style = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    language = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255)
+    ch_lyrics = models.TextField()
+    en_lyrics = models.TextField()
+    rating = models.CharField(max_length=255)
+    views = models.IntegerField()
+    release_year = models.IntegerField()
+    publish_time = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ('-pub_date' , )
 
     def __str__(self):
         return f"{self.title}  / {self.song_ID} / ({self.pub_date})"
+    
+def create_tables(ob):
+    ob.objects.create_table()
