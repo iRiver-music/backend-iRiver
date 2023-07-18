e68afa41016e19dd2476026e06cb29616a5113f2
weizijun <zijun1152@gmail.com>
2023-07-18 22:05:35 +0800

    Fixed & Feat: fixed wrong in Music/admin.py and add clear_str.py into Music

--- Music/admin.py
+++ Music/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Post, music
+from .models import Artist, Music
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(music)
+admin.site.register(Artist)
+admin.site.register(Music)
