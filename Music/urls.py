b456226964f1f19160a656ad252292ac63577ce4
weizijun <zijun1152@gmail.com>
2023-07-24 20:24:23 +0800

    Fixed : fixed the path to same with api document

--- Music/urls.py
+++ Music/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'Music'
 urlpatterns = [
 
-    path('query_db_song/<str:query>/', views.query_db_song, name='query_db_song'),
-    path('query_web_song/<str:query>/', views.query_web_song, name='query_web_song'),
+    path('search/query_db_song/<str:query>/', views.query_db_song, name='query_db_song'),
+    path('search/query_web_song/<str:query>/', views.query_web_song, name='query_web_song'),
     path('get_artist_info/<str:artist>/', views.get_artist_info, name='get_artist_info'),
-    path('query_album/<str:be_search_album>/', views.query_album, name='query_album'), 
+    path('search/query_album/<str:be_search_album>/', views.query_album, name='query_album'), 
 ]
