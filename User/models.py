8829165f8e4c7c9334450544f71dcfb7d14d6725
lai hung wei <128201642+s990093@users.noreply.github.com>
2023-07-28 03:34:25 +0800

    modify: user

--- User/models.py
+++ User/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
 # 國家清單
-COUNTRY_CHOICES=(
+COUNTRY_CHOICES = (
     ("NN", "不透露"),
     ("US", "United States"),
     ("CA", "Canada"),
@@ -20,78 +20,82 @@ COUNTRY_CHOICES=(
     ("ID", "Indonesia"),
 )
 # 性別清單
-GENDER={
+GENDER = {
     ("U", "不透露"),
     ("M", "男"),
     ("F", "女"),
 }
 
+
 def custom_default_date():
     # Custom logic to set the default date and time
     return timezone.now()  # You can replace this with your own logic if needed
 
+
 class Profile(models.Model):
-    id=models.CharField(max_length=36, primary_key=True)
-    email=models.CharField(max_length=24)
-    username=models.CharField(max_length=24)
-    phone=models.CharField(max_length=16)
-    country=models.CharField(max_length=2, null=True)
-    birthday=models.DateField(null=True)
-    gender=models.CharField(max_length=1, null=True)
-    user_img_url=models.CharField(max_length=255, null=True)
-    level=models.PositiveSmallIntegerField(default=0)
-    created_at=models.DateTimeField(default=custom_default_date)
+    id = models.CharField(max_length=36, primary_key=True)
+    email = models.CharField(max_length=24)
+    username = models.CharField(max_length=24)
+    phone = models.CharField(max_length=16)
+    country = models.CharField(max_length=2, null=True)
+    birthday = models.DateField(null=True)
+    gender = models.CharField(max_length=1, null=True)
+    user_img_url = models.CharField(max_length=255, null=True)
+    level = models.PositiveSmallIntegerField(default=0)
+    created_at = models.DateTimeField(default=custom_default_date)
 
     class Meta:
-        app_label="User"
-        db_table="profile"
+        app_label = "User"
+        db_table = "profile"
+
 
 class Setting(models.Model):
-    id=models.CharField(max_length=36, primary_key=True)
-    LANGUAGE=models.CharField(max_length=255)
-    SHOW_MODAL=models.CharField(max_length=255)
-    AUDIO_QUALITY=models.CharField(max_length=255)
-    AUDIO_AUTO_PLAY=models.BooleanField()
-    WIFI_AUTO_DOWNLOAD=models.BooleanField()
-    created_at=models.DateTimeField(default=custom_default_date)
+    id = models.CharField(max_length=36, primary_key=True)
+    LANGUAGE = models.CharField(max_length=255)
+    SHOW_MODAL = models.CharField(max_length=255)
+    AUDIO_QUALITY = models.CharField(max_length=255)
+    AUDIO_AUTO_PLAY = models.BooleanField()
+    WIFI_AUTO_DOWNLOAD = models.BooleanField()
+    created_at = models.DateTimeField(default=custom_default_date)
 
     class Meta:
-        app_label="User"
-        db_table="setting"
+        app_label = "User"
+        db_table = "setting"
+
 
 class EQ(models.Model):
-    id=models.CharField(max_length=36, primary_key=True)
-    ENGANCE_HIGH=models.BooleanField(null=True, blank=True)
-    ENGANCE_MIDDLE=models.BooleanField(null=True, blank=True)
-    ENGANCE_LOW=models.BooleanField(null=True, blank=True)
-    ENGANCE_HEAVY=models.BooleanField(null=True, blank=True)
-    STYLE=models.CharField(max_length=255, null=True, blank=True)
-    EQ_HIGH=models.IntegerField(null=True)
-    EQ_MIDDLE=models.IntegerField(null=True)
-    EQ_LOW=models.IntegerField(null=True)
-    EQ_HEAVY=models.IntegerField(null=True)
-    EQ_DISTORTION=models.IntegerField(null=True)
-    EQ_ZIP=models.IntegerField(null=True)
-    SPATIAL_AUDIO=models.CharField(max_length=255, null=True, blank=True)
-    _60HZ=models.IntegerField(null=True)
-    _230HZ=models.IntegerField(null=True)
-    _910HZ=models.IntegerField(null=True)
-    _4kHZ=models.IntegerField(null=True)
-    _14kHZ=models.IntegerField(null=True)
-    created_at=models.DateTimeField(default=custom_default_date)
+    id = models.CharField(max_length=36, primary_key=True)
+    ENGANCE_HIGH = models.BooleanField(null=True, blank=True)
+    ENGANCE_MIDDLE = models.BooleanField(null=True, blank=True)
+    ENGANCE_LOW = models.BooleanField(null=True, blank=True)
+    ENGANCE_HEAVY = models.BooleanField(null=True, blank=True)
+    STYLE = models.CharField(max_length=255, null=True, blank=True)
+    EQ_HIGH = models.IntegerField(null=True)
+    EQ_MIDDLE = models.IntegerField(null=True)
+    EQ_LOW = models.IntegerField(null=True)
+    EQ_HEAVY = models.IntegerField(null=True)
+    EQ_DISTORTION = models.IntegerField(null=True)
+    EQ_ZIP = models.IntegerField(null=True)
+    SPATIAL_AUDIO = models.CharField(max_length=255, null=True, blank=True)
+    _60HZ = models.IntegerField(null=True)
+    _230HZ = models.IntegerField(null=True)
+    _910HZ = models.IntegerField(null=True)
+    _4kHZ = models.IntegerField(null=True)
+    _14kHZ = models.IntegerField(null=True)
+    created_at = models.DateTimeField(default=custom_default_date)
 
     class Meta:
-        app_label="User"
-        db_table="eq"
+        app_label = "User"
+        db_table = "eq"
+
 
 class Social(models.Model):
-    id=models.CharField(max_length=36, primary_key=True)
-    email=models.CharField(max_length=24)
-    uid=models.CharField(max_length=24)
-    created_at=models.DateTimeField(default=custom_default_date)
+    id = models.CharField(max_length=36, primary_key=True)
+    email = models.CharField(max_length=24)
+    uid = models.CharField(max_length=24)
+    created_at = models.DateTimeField(default=custom_default_date)
 
     class Meta:
-<<<<<<< HEAD
         app_label = "User"
         db_table = "social"
 #
@@ -107,16 +111,3 @@ class Playlist(models.Model):
     class Meta:
         app_label = "User"
         db_table = "playlist"
-=======
-        app_label="User"
-        db_table="social"
-
-class musicList(models.Model):
-    playlist=models.CharField(max_length=255)
-    music_ID=models.CharField(max_length=32)
-    favorite=models.PositiveSmallIntegerField(default=0)
-    created_at=models.DateTimeField(default=custom_default_date)
-
-    class Meta:
-        app_label="User"
->>>>>>> 4b657f735fae7a3dcc73711850eae71dffcef26b
