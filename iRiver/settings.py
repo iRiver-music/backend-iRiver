7af27a8f33df4e91615e696ceee68769020fa894
lai hung wei <128201642+s990093@users.noreply.github.com>
2023-07-28 04:10:50 +0800

    modify: music   db name change

--- iRiver/settings.py
+++ iRiver/settings.py
@@ -139,20 +139,31 @@ WSGI_APPLICATION = 'iRiver.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'music',
-    #     'USER': 'gWvPZkyaanAP5cXQqE8hkX5hnmYYhcMr',
-    #     'PASSWORD': 'JABmQsQhpj05F6WI',
-    #     'HOST': '49.213.238.75',
-    #     'PORT': '3306',
-    #     'OPTIONS': {
-    #         'sql_mode': 'STRICT_ALL_TABLES',
-    #     }
-    # },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music',
+        'USER': 'gWvPZkyaanAP5cXQqE8hkX5hnmYYhcMr',
+        'PASSWORD': 'JABmQsQhpj05F6WI',
+        'HOST': '49.213.238.75',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'STRICT_ALL_TABLES',
+        }
+    },
+    'music': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music',
+        'USER': 'gWvPZkyaanAP5cXQqE8hkX5hnmYYhcMr',
+        'PASSWORD': 'JABmQsQhpj05F6WI',
+        'HOST': '49.213.238.75',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'STRICT_ALL_TABLES',
+        }
+    },
     'test': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'music_db',
+        'NAME': 'music',
         'USER': 'gWvPZkyaanAP5cXQqE8hkX5hnmYYhcMr',
         'PASSWORD': 'JABmQsQhpj05F6WI',
         'HOST': '49.213.238.75',
@@ -217,17 +228,17 @@ DATABASES = {
             'sql_mode': 'STRICT_ALL_TABLES',
         }
     },
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'music',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'STRICT_ALL_TABLES',
-        }
-    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'music',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    #     'OPTIONS': {
+    #         'sql_mode': 'STRICT_ALL_TABLES',
+    #     }
+    # },
 
 }
 
