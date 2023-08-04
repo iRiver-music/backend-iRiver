e05f77b0c5c8ddbc9150b95c364fa6d4e7a790af
林立純 <allen@linlichundeMacBook-Pro.local>
2023-08-04 19:01:42 +0800

    Feat: CSRF token
    -更改Token

--- Token/urls.py
+++ Token/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path ,include
 from . import views
-from .views import UserTokenObtainPairView
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
 
 app_name = 'Token'
 urlpatterns = [
-    path('token/', UserTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', views.get_csrf_token, name='get_token'),
+    path('token/api', views.some_api_endpoint, name='api_endpoint'),
 ]
--- Token/views.py
+++ Token/views.py
@@ -1,15 +1,22 @@
-from rest_framework.response import Response
+from django.middleware.csrf import get_token
 from rest_framework.decorators import api_view
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
 
-#自定義Token Serializer
-class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        token['username'] = user.username
-        return token
+# 這個 API 視圖負責在每次請求時生成並返回新的 CSRF token
+@api_view(['GET'])
+def get_csrf_token(request):
+    # 使用 Django 提供的 get_token 方法生成新的 CSRF token
+    new_csrf_token = get_token(request)
 
-class UserTokenObtainPairView(TokenObtainPairView):
-    serializer_class = UserTokenObtainPairSerializer
+    # 在 Response 的 headers 中設置新的 CSRF token，讓前端可以獲取並更新
+    response = Response({'csrfToken': new_csrf_token})
+    response['X-CSRFToken'] = new_csrf_token
+    return response
+
+# 這個 API 視圖用於處理其他請求，你可以將它視為一個示例
+@api_view(['POST'])
+def some_api_endpoint(request):
+    # 在這裡處理其他 POST 請求的邏輯
+    # 這裡只是一個示例，你可以根據實際需求來處理 POST 請求的邏輯
+
+    return Response({'message': 'Success'})
