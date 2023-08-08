a608ce7692b3ee332f38f7669e6d1b6a25de4081
chris ho <98445729+ho0527@users.noreply.github.com>
2023-08-08 22:24:11 +0800

    modify: user/model and setting

--- iRiver/settings.py
+++ iRiver/settings.py
@@ -170,28 +170,6 @@ DATABASES = {
             'sql_mode': 'STRICT_ALL_TABLES',
         }
     },
-    'user': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'iriver_user_data',
-        'USER': 'gWvPZkyaanAP5cXQqE8hkX5hnmYYhcMr',
-        'PASSWORD': 'ZkyaanAP5cXQqE8hkX5hnmYYhcMr',
-        'HOST': '49.213.238.75',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'STRICT_ALL_TABLES',
-        }
-    },
-    'usermusic': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'iriver_user_music_list',
-        'USER': 'gWvPZkyaanAP5cXQqE8hkX5hnmYYhcMr',
-        'PASSWORD': 'ZkyaanAP5cXQqE8hkX5hnmYYhcMr',
-        'HOST': '49.213.238.75',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'STRICT_ALL_TABLES',
-        }
-    },
     # localhost
 
     # 'default': {
