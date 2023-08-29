cb0c2dfa41516dab0d8fc55bc2eceb3d6d25b2fa
weizijun <zijun1152@gmail.com>
2023-08-29 10:42:17 +0800

    Modify : To optimize manage of RAM -將query中，只向檔案中讀取需要的四個參數，而不是將19個參數全部讀取出來，避免因流量過大而導致的內存溢出

--- Music/query.py
+++ Music/query.py
@@ -2,6 +2,7 @@ import concurrent.futures
 import json
 import os
 import glob
+import ijson
 from fuzzywuzzy import fuzz
 from django.conf import settings
 from django.http import JsonResponse
@@ -43,14 +44,39 @@ def style_search(style, query):
     return fuzz.ratio(query, style['style']) + fuzz.ratio(query, style['title'])
 
 
+def read_file(file_name) : 
+    results = []
+    current_dict = {}
+    keys_to_extract = ['title', 'artist', 'album', 'music_ID']
+    with open(file_name, 'rb') as file :
+        parser = ijson.parse(file)
+        in_data_array = False
+        for prefix, event, value in parser :
+            if in_data_array:
+                if event == 'map_key' :
+                    current_key = value
+                elif event == 'string' and current_key in keys_to_extract :
+                    current_dict[current_key] = value
+                    if len(current_dict) == len(keys_to_extract):
+                        results.append(current_dict)
+                        current_dict = {}
+            elif prefix == 'data' and event == 'start_array' :
+                in_data_array = True
+            elif prefix == 'data' and event == 'end_array' :
+                in_data_array = False
+                break
+    return results
+
 def search_music(file_name, query) -> list:
 
     # open json file
-    with open(file_name, 'r') as jsonFile:
-        song_json = json.load(jsonFile)
+    # with open(file_name, 'r') as jsonFile:
+    #     song_json = json.load(jsonFile)
 
-    song_list = song_json['data']
+    # song_list = song_json['data']
     # use radio algorithnm in fuzzywuzzy to sort music by rate (high to low)
+
+    song_list = read_file(file_name)
 
     results = sorted(song_list, key=lambda song: song_search(
         song, query), reverse=True)
@@ -65,10 +91,13 @@ def search_music(file_name, query) -> list:
 def search_artist(file_name, query) -> list:
 
     # open json file
-    with open(file_name, 'r') as jsonFile:
-        song_json = json.load(jsonFile)
+    # with open(file_name, 'r') as jsonFile:
+    #     song_json = json.load(jsonFile)
 
-    song_list = song_json['data']
+    # song_list = song_json['data']
+
+    song_list = read_file(file_name)
+
     # use radio algorithnm in fuzzywuzzy to sort music by rate (high to low)
 
     results = sorted(song_list, key=lambda song: artist_search(
@@ -84,10 +113,13 @@ def search_artist(file_name, query) -> list:
 def search_album(file_name, query) -> list:
 
     # open json file
-    with open(file_name, 'r') as jsonFile:
-        song_json = json.load(jsonFile)
+    # with open(file_name, 'r') as jsonFile:
+    #     song_json = json.load(jsonFile)
 
-    song_list = song_json['data']
+    # song_list = song_json['data']
+
+    song_list = read_file(file_name)
+
     # use radio algorithnm in fuzzywuzzy to sort music by rate (high to low)
 
     results = sorted(song_list, key=lambda song: aldum_search(
@@ -103,10 +135,13 @@ def search_album(file_name, query) -> list:
 def search_style(file_name, query) -> list:
 
     # open json file
-    with open(file_name, 'r') as jsonFile:
-        song_json = json.load(jsonFile)
+    # with open(file_name, 'r') as jsonFile:
+    #     song_json = json.load(jsonFile)
 
-    song_list = song_json['data']
+    # song_list = song_json['data']
+
+    song_list = read_file(file_name)
+
     # use radio algorithnm in fuzzywuzzy to sort music by rate (high to low)
 
     results = sorted(song_list, key=lambda song: style_search(
@@ -122,10 +157,13 @@ def search_style(file_name, query) -> list:
 def search_artist(file_name, query) -> list:
 
     # open json file
-    with open(file_name, 'r') as jsonFile:
-        song_json = json.load(jsonFile)
+    # with open(file_name, 'r') as jsonFile:
+    #     song_json = json.load(jsonFile)
 
-    song_list = song_json['data']
+    # song_list = song_json['data']
+
+    song_list = read_file(file_name)
+
     # use radio algorithnm in fuzzywuzzy to sort music by rate (high to low)
 
     results = sorted(song_list, key=lambda song: artist_search(
@@ -141,10 +179,13 @@ def search_artist(file_name, query) -> list:
 def search_album(file_name, query) -> list:
 
     # open json file
-    with open(file_name, 'r') as jsonFile:
-        song_json = json.load(jsonFile)
+    # with open(file_name, 'r') as jsonFile:
+    #     song_json = json.load(jsonFile)
 
-    song_list = song_json['data']
+    # song_list = song_json['data']
+
+    song_list = read_file(file_name)
+
     # use radio algorithnm in fuzzywuzzy to sort music by rate (high to low)
 
     results = sorted(song_list, key=lambda song: aldum_search(
@@ -160,10 +201,13 @@ def search_album(file_name, query) -> list:
 def search_style(file_name, query) -> list:
 
     # open json file
-    with open(file_name, 'r') as jsonFile:
-        song_json = json.load(jsonFile)
+    # with open(file_name, 'r') as jsonFile:
+    #     song_json = json.load(jsonFile)
 
-    song_list = song_json['data']
+    # song_list = song_json['data']
+
+    song_list = read_file(file_name)
+
     # use radio algorithnm in fuzzywuzzy to sort music by rate (high to low)
 
     results = sorted(song_list, key=lambda song: style_search(
@@ -269,7 +313,7 @@ def multiprocess_style(query, music_file_list) -> list:
 def query(query):
     # manager = Manager()
     # search_results = manager.list()
-    try:
+    try :
         music_file_list = count_music_file()
         style_file_list = count_style_file()
 
@@ -305,12 +349,21 @@ def query(query):
         del album_results[-1]
         del style_results[-1]
 
+        # 將每個陣列的前五筆資料取平均分數，將分數最大的項回傳，告訴前端此項優先顯示。
+        # 測試演算法分數，設定一個閾值，如果分數最大項與分數第二大項平均分數相差超過閾值，則做關聯性比較（例：如果查詢稻香，
+        # song的分數比artist高出閾值，則將song第一項對應的aritst項（周杰倫）推進artist list的第一項，style不用做）。
+        # 現在是將每項的19筆資料存進來，有內存隱患，改為將每項的4筆資料傳進來，分別為title, artist, album, ID。
+
         data = {
-            'song': music_results,
-            'artist': artist_results,
-            'album': album_results,
-            'style': style_results,
+            'song' : music_results,
+            'artist' : artist_results,
+            'album' : album_results,
+            'style' : style_results,
         }
+
+        
+
+        print('finish second sort')
     except Exception as e:
         return {'error in query': str(e)}
 
