5a3b15eef3ed2d03c2a33893e37a98d2c66f5919
lai hung wei <128201642+s990093@users.noreply.github.com>
2023-08-31 14:00:59 +0800

    Modify : change query

--- Music/query-old.py
+++ Music/query-old.py
@@ -17,61 +17,64 @@ from .serializers import SongSerializer
 from multiprocessing import Process, Manager
 
 
-def count_file() -> list : 
-    absolute_path = os.path.join(settings.BASE_DIR, 'Music', 'music_db','*.json')
+def count_file() -> list:
+    absolute_path = os.path.join(
+        settings.BASE_DIR, 'Music', 'music_db', '*.json')
     json_files = glob.glob(absolute_path)
 
     return json_files
 
-def song_search(song, query):
-        return fuzz.ratio(query, song['title'])
 
-def search_music(file_name, query, results_list) -> list : 
+def song_search(song, query):
+    return fuzz.ratio(query, song['title'])
+
+
+def search_music(file_name, query, results_list) -> list:
 
     # open json file
-    with open(file_name, 'r') as jsonFile : 
+    with open(file_name, 'r') as jsonFile:
         song_json = json.load(jsonFile)
 
     song_list = song_json['data']
     # use radio algorithnm in fuzzywuzzy to sort music by rate (high to low)
-    
-    results = sorted(song_list, key=lambda song: song_search(song, query), reverse=True)
+
+    results = sorted(song_list, key=lambda song: song_search(
+        song, query), reverse=True)
     results = results[:20]
     # print(type(results))
     # print(results)
-    # push music_list into the querylist 
+    # push music_list into the querylist
     print("finish sorted")
     results_list.append(results)
 
-def new_query(request, query) : 
+
+def new_query(request, query):
     processes = []
     manager = Manager()
     first_results = manager.list()
-    try :
+    try:
         file_list = count_file()
         print('file_list : ', file_list)
         # search_music(file_list[0], query, processes)
         # print(processes)
-        for file in file_list : 
+        for file in file_list:
             p = Process(target=search_music, args=(file, query, first_results))
             processes.append(p)
             p.start()
 
-        for p in processes : 
+        for p in processes:
             p.join()
         search_list = []
-        for result_list in first_results : 
-            for i in result_list : 
+        for result_list in first_results:
+            for i in result_list:
                 search_list.append(i)
 
-        results = sorted(search_list, key=lambda song: song_search(song, query), reverse=True)
+        results = sorted(search_list, key=lambda song: song_search(
+            song, query), reverse=True)
         print('finish second sort')
     except Exception as e:
         print('Error in new_query : ')
         print(e)
         return JsonResponse({'error': str(e)}, status=500)
-    
-    return JsonResponse({'data' : results})
 
-
-
+    return JsonResponse({'data': results})
